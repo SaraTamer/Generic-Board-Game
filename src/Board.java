e67fc6e847a179131d7dd49cc5e37086132034d0
SaraTamer <sasooelbihery@gmail.com>
2023-02-18 13:58:46 +0200

    Start

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+public class Board
+{
+    private int dimension;
+    private char board[][] = new char[dimension][dimension];
+
+    public void setDimension(char dimension) {
+        this.dimension = dimension;
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player
+{
+    private char symbol;
+
+    public void setSymbol(char symbol)
+    {
+        this.symbol = symbol;
+    }
+}
